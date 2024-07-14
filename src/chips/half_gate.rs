db4064f77c783e552be79a9f028b3f494507736c
tkmct <1220t.takamichi@gmail.com>
2024-07-14 22:33:47 +0900

    [WIP] test half-gate

--- src/chips/half_gate.rs
+++ src/chips/half_gate.rs
@@ -29,6 +29,7 @@ impl HalfGateChip {
         selector: Selector,
     ) -> HalfGateConfig {
         // TODO: setup custom gates for these.
+        meta.enable_equality(advice);
 
         HalfGateConfig {
             advice,
@@ -39,15 +40,15 @@ impl HalfGateChip {
     pub fn generate_half_gate(
         &self,
         layouter: &mut impl Layouter<Fp>,
-        x_label: &[AssignedCell<Fp, Fp>],
-        y_label: &[AssignedCell<Fp, Fp>],
+        x_0: &[AssignedCell<Fp, Fp>],
+        y_0: &[AssignedCell<Fp, Fp>],
         delta: &[AssignedCell<Fp, Fp>],
     ) -> Result<Vec<AssignedCell<Fp, Fp>>, Error> {
         // Assert label and delta length.
         // Block is [u8;16]. So it uses 16 cells -> 128bit values
         // Label, Delta are both Block
-        assert_eq!(x_label.len(), 16);
-        assert_eq!(y_label.len(), 16);
+        assert_eq!(x_0.len(), 16);
+        assert_eq!(y_0.len(), 16);
         assert_eq!(delta.len(), 16);
 
         layouter.assign_region(
@@ -57,8 +58,7 @@ impl HalfGateChip {
                 self.config.q.enable(&mut region, offset)?;
 
                 // Copy x zero label to this region
-                x_label
-                    .iter()
+                x_0.iter()
                     .map(|x| {
                         let cell = x.copy_advice(
                             || "Copy X label",
@@ -70,6 +70,13 @@ impl HalfGateChip {
                         cell
                     })
                     .collect::<Result<Vec<_>, Error>>()?;
+
+                // let x_1 = x_0 ^ delta;
+                // let y_1 = y_0 ^ delta;
+
+                // get color bit of x_0 and y_0.
+                // let color_x = x_0.lsb();
+                // let color_y = y_0.lsb();
 
                 // NOTE: make hash function swappable
                 // TODO: handle tccr
@@ -85,7 +92,7 @@ impl HalfGateChip {
 mod tests {
     use super::*;
     use crate::halo2_proofs::{
-        circuit::{Layouter, SimpleFloorPlanner},
+        circuit::{Layouter, SimpleFloorPlanner, Value},
         dev::MockProver,
         halo2curves::bn256::Fr as Fp,
         plonk::{Circuit, ConstraintSystem},
@@ -113,11 +120,67 @@ mod tests {
         fn synthesize(
             &self,
             config: Self::Config,
-            layouter: &mut impl Layouter<Fp>,
+            mut layouter: impl Layouter<Fp>,
         ) -> Result<(), Error> {
             let chip = HalfGateChip::construct(config);
 
-            let out = chip.generate_half_gate(&mut layouter, x_label, y_label, delta)?;
+            // Assign x and y to advice column
+            let (x_label, y_label, delta) = layouter.assign_region(
+                || "Assign values",
+                |mut region| {
+                    let mut offset = 0;
+                    let x_label = self
+                        .x
+                        .to_bytes()
+                        .iter()
+                        .map(|byte| {
+                            let res = region.assign_advice(
+                                || "assign x label",
+                                config.advice,
+                                offset,
+                                || Value::known(Fp::from(*byte as u64)),
+                            );
+                            offset += 1;
+                            res
+                        })
+                        .collect::<Result<Vec<_>, Error>>()?;
+                    let y_label = self
+                        .y
+                        .to_bytes()
+                        .iter()
+                        .map(|byte| {
+                            let res = region.assign_advice(
+                                || "assign y label",
+                                config.advice,
+                                offset,
+                                || Value::known(Fp::from(*byte as u64)),
+                            );
+                            offset += 1;
+                            res
+                        })
+                        .collect::<Result<Vec<_>, Error>>()?;
+
+                    let delta = self
+                        .delta
+                        .to_bytes()
+                        .iter()
+                        .map(|byte| {
+                            let res = region.assign_advice(
+                                || "assign delat",
+                                config.advice,
+                                offset,
+                                || Value::known(Fp::from(*byte as u64)),
+                            );
+                            offset += 1;
+                            res
+                        })
+                        .collect::<Result<Vec<_>, Error>>()?;
+
+                    Ok((x_label, y_label, delta))
+                },
+            )?;
+
+            let out = chip.generate_half_gate(&mut layouter, &x_label, &y_label, &delta)?;
 
             Ok(())
         }
